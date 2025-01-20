30bc0f8f5426c430d68a4a728557b035a42fcd8f
Patrick Blondel <pblondel@users.noreply.github.com>
2025-01-20 18:31:27 +0000

    Update streamlit_app.py
    Make api call to return json response

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,4 +37,4 @@ if ingredients_list:
 # Display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
