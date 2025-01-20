07fe09cb7a5ba0b0b2f609d3e971e45f8c3f3f7d
Patrick Blondel <pblondel@users.noreply.github.com>
2025-01-20 18:33:58 +0000

    Update streamlit_app.py
    Adding a dataframe to store api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,4 +37,4 @@ if ingredients_list:
 # Display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
